fbde4f3457729760a33e5fa83d201c2d072b45e0
humza <ahmadhumza7@gmail.com>
2024-12-26 21:25:01 -0800

    Removed duplicate code

--- feature-items/src/main/java/humzaahmad/fetchhumzaahmadtakehome/feature/items/ItemsScreen.kt
+++ feature-items/src/main/java/humzaahmad/fetchhumzaahmadtakehome/feature/items/ItemsScreen.kt
@@ -3,6 +3,7 @@ package humzaahmad.fetchhumzaahmadtakehome.feature.items
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -22,15 +24,29 @@ fun ItemsScreen(modifier: Modifier, viewModel: ItemsViewModel = hiltViewModel())
             ItemsScreen(modifier = modifier, groups = (uiState.value as ItemsScreenState.Success).groups)
         }
         is ItemsScreenState.Error -> {
-            Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                Text(text = (uiState.value as ItemsScreenState.Error).message)
+            ItemsScreenColumn(modifier){
+                Text(modifier = Modifier.padding(16.dp), text = (uiState.value as ItemsScreenState.Error).message)
             }
         }
         ItemsScreenState.Loading -> {
-            Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            ItemsScreenColumn(modifier = modifier) {
                 CircularProgressIndicator()
             }
         }
+    }
+}
+
+@Composable
+private fun ItemsScreenColumn(
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        content()
     }
 }
 
